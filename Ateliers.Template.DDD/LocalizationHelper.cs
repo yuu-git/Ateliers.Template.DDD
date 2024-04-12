fe986b9fb9e76b07fded432fd90b45569d9660b0
Yuu.Konno <yuu.developer@gmail.com>
2024-04-13 08:07:30 +0900

    プロジェクト ファイルを追加します。

---
+++ Ateliers.Template.DDD/LocalizationHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Resources;
+using System.Text;
+
+namespace Ateliers.Template.DDD
+{
+    internal static class LocalizationHelper
+    {
+        private static ResourceManager rm = new ResourceManager("Ateliers.Template.DDD", typeof(LocalizationHelper).Assembly);
+
+        internal static string GetString(string key)
+        {
+            CultureInfo culture = CultureInfo.CurrentUICulture;
+            return rm.GetString(key, culture);
+        }
+    }
+
+}
